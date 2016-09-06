fc5314d910afe438271a9fbff6356fa46c02f50e
Nicolai <nico8829@edu.eal.dk>
2016-09-06 11:46:26 +0200

    wip

---
+++ 02_Bottles/Song.cs
@@ -0,0 +1,28 @@
+﻿namespace _02_Bottles
+{
+    internal class Song
+    {
+        public Song()
+        {
+        }
+        internal string CountBottles (int bottles)
+        {
+            string mySong = "";
+            {
+                for (int i = bottles; i > 0; i--)
+                { 
+                 if (i == 0)
+                    {
+                        mySong = mySong + " No more bottles of beer on the wall.";
+                    }
+                  else if (i == 1)
+                    {
+                        mySong = mySong + "1 bottle of beer on the wall.";
+                    }
+                    else
+                    {
+
+                    }
+        }
+    }
+}
